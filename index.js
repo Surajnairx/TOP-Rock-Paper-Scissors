490b65397e2a35e4f9d1a30460a8201bc8fa4c7d
Rinzler <Rinzler@123>
2024-03-27 18:14:04 +0530

    Setting up an event listener for button clicks

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const programOptions = ["rock", "paper", "scissor"];
+let numberOfButtons = document.querySelectorAll(".button").length;
+
+let buttonId;
+for (let i = 0; i < numberOfButtons; i++) {
+  document.querySelectorAll(".button")[i].addEventListener("click", (event) => {
+    buttonId = event.target.id;
+    playRound(buttonId, getProgramChoice);
+  });
+}
 
 function getProgramChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
