2f6982b8ccff7106793207d68f39e9f0cb43d120
Rinzler <Rinzler@123>
2024-03-27 18:16:47 +0530

    Main function for game execution

--- index.js
+++ index.js
@@ -14,3 +14,89 @@ function getProgramChoice() {
   let randomChoice = programOptions[randomNumber];
   return randomChoice;
 }
+
+function playRound(userSelection, programSelection) {
+  let program = programSelection();
+
+  console.log(userSelection);
+  console.log(program);
+  // for rock
+  if (userSelection == "rock") {
+    document
+      .querySelector(".user")
+      .childNodes[1].setAttribute("src", `./images/${userSelection}.png`);
+    if (program == "rock") {
+      document
+        .querySelector(".program")
+        .childNodes[1].setAttribute("src", `./images/${program}.png`);
+      document.querySelector(
+        ".heading"
+      ).childNodes[1].innerHTML = `IT'S A DRAW`;
+    } else if (program == "paper") {
+      document
+        .querySelector(".program")
+        .childNodes[1].setAttribute("src", `./images/${program}.png`);
+      document.querySelector(".heading").childNodes[1].innerHTML =
+        "DAMMN IT, WE LOST!!";
+    } else if (program == "scissor") {
+      document
+        .querySelector(".program")
+        .childNodes[1].setAttribute("src", `./images/${program}.png`);
+      document.querySelector(".heading").childNodes[1].innerHTML =
+        "VICTORY IS OURS!!";
+    }
+  }
+  // for paper
+  if (userSelection == "paper") {
+    document;
+    document
+      .querySelector(".user")
+      .childNodes[1].setAttribute("src", `./images/${userSelection}.png`);
+    if (program == "rock") {
+      document
+        .querySelector(".program")
+        .childNodes[1].setAttribute("src", `./images/${program}.png`);
+      document.querySelector(
+        ".heading"
+      ).childNodes[1].innerHTML = `VICTORY IS OURS!!`;
+    } else if (program == "paper") {
+      document
+        .querySelector(".program")
+        .childNodes[1].setAttribute("src", `./images/${program}.png`);
+      document.querySelector(".heading").childNodes[1].innerHTML =
+        "IT'S A DRAW";
+    } else if (program == "scissor") {
+      document
+        .querySelector(".program")
+        .childNodes[1].setAttribute("src", `./images/${program}.png`);
+      document.querySelector(".heading").childNodes[1].innerHTML =
+        "DAMMN IT, WE LOST!!";
+    }
+  }
+  // for scissor
+  if (userSelection == "scissor") {
+    document
+      .querySelector(".user")
+      .childNodes[1].setAttribute("src", `./images/${userSelection}.png`);
+    if (program == "rock") {
+      document
+        .querySelector(".program")
+        .childNodes[1].setAttribute("src", `./images/${program}.png`);
+      document.querySelector(
+        ".heading"
+      ).childNodes[1].innerHTML = `DAMMN IT, WE LOST!!`;
+    } else if (program == "paper") {
+      document
+        .querySelector(".program")
+        .childNodes[1].setAttribute("src", `./images/${program}.png`);
+      document.querySelector(".heading").childNodes[1].innerHTML =
+        "VICTORY IS OURS!!";
+    } else if (program == "scissor") {
+      document
+        .querySelector(".program")
+        .childNodes[1].setAttribute("src", `./images/${program}.png`);
+      document.querySelector(".heading").childNodes[1].innerHTML =
+        "IT'S A DRAW";
+    }
+  }
+}
